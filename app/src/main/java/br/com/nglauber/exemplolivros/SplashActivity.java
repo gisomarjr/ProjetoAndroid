9a2ddc06b3fe582741ec92a2632d95130db8e4d3
markosbastos <markosbastos@gmail.com>
2015-05-24 16:09:13 -0300

    Inserindo Splash activity e corrigindo o início.

---
+++ app/src/main/java/br/com/nglauber/exemplolivros/SplashActivity.java
@@ -0,0 +1,30 @@
+package br.com.nglauber.exemplolivros;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class SplashActivity extends Activity {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_splash);
+
+            new Timer().schedule(new TimerTask() {
+
+                @Override
+                public void run() {
+                    finish();
+
+                    Intent intent = new Intent();
+                    intent.setClass(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }, 6000);
+        }
+}
